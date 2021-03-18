f5179e81dd0cfda9a9b629cc419a2a3560b827c5
Deepanshu Tiwari <ddepu11@gmail.com>
2021-03-18 17:26:49 +0530

    Sidebar added and styled for mobile view

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,30 +1,51 @@
-import React from "react";
+import React, { useRef } from "react";
 import { Link } from "react-router-dom";
 import logo from "../images/logo.png";
+import { FaBars, FaRegWindowClose } from "react-icons/fa";
 
 export default function Navbar() {
+  const navRef = useRef(null);
+
+  function handleMenu() {
+    if (navRef.current.classList.contains("nav-show")) {
+      navRef.current.classList.remove("nav-show");
+    } else {
+      navRef.current.classList.add("nav-show");
+    }
+  }
+
   return (
     <nav className="navbar">
-      <ul className="nav-ul flex">
+      <ul ref={navRef} className="nav-ul flex">
         <li>
-          <Link to="/">Home</Link>
+          <Link onClick={handleMenu} to="/">
+            Home
+          </Link>
         </li>
 
         <li>
-          <Link to="/about">About</Link>
+          <Link onClick={handleMenu} to="/about">
+            About
+          </Link>
         </li>
         <li>
-          <Link to="/">
+          <Link onClick={handleMenu} to="/">
             <img src={logo} alt="" />
           </Link>
         </li>
         <li>
-          <Link to="/contact">Contact Us</Link>
+          <Link onClick={handleMenu} to="/contact">
+            Contact Us
+          </Link>
         </li>
         <li>
-          <Link to="/services">Services</Link>
+          <Link onClick={handleMenu} to="/services">
+            Services
+          </Link>
         </li>
+        <FaRegWindowClose className="close" onClick={handleMenu} />
       </ul>
+      <FaBars className="menu" onClick={handleMenu} />
     </nav>
   );
 }
