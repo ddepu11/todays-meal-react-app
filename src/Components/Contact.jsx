77f4201009e8349726cd61a598c6cea2cf1f7c3c
Deepanshu Tiwari <ddepu11@gmail.com>
2021-03-18 14:33:01 +0530

    contact page html done

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,9 +1,41 @@
 import React from "react";
+import {
+  FaFacebookSquare,
+  FaTwitterSquare,
+  FaGithubSquare,
+  FaInstagramSquare,
+} from "react-icons/fa";
 
 export default function Contact() {
   return (
     <div>
-      <h1>Contact</h1>
+      <header>
+        <h1>Contact Us</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Error animi
+          pariatur laborum ducimus, autem veniam iusto excepturi cum numquam
+        </p>
+      </header>
+      <aside className="aside">
+        <div>
+          <h3>Address</h3>
+          <span>utem veniam iusto excepturi cum numquam</span>
+        </div>
+        <div>
+          <h3>Email</h3>
+          <span>xyz_mno@gmail.com</span>
+        </div>
+      </aside>
+      <section className="section">
+        <h3>Phone</h3>
+        <span>15769+895</span>
+        <div className="social-links">
+          <FaGithubSquare />
+          <FaFacebookSquare />
+          <FaTwitterSquare />
+          <FaInstagramSquare />
+        </div>
+      </section>
     </div>
   );
 }
